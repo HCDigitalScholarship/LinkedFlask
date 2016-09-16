eb12126ad79cf8d7e3f86d0913f85faae4a0ee84
Maddy <mfhodges@haverford.edu>
2016-09-16 13:20:39 -0400

    recent changes

--- hello.py
+++ hello.py
@@ -5,11 +5,15 @@ from flask import render_template
 
 @app.route('/person/')
 @app.route('/person/<name>')
-def hello(name=None):
+def hello(name=None, birth=None, death=None, par1=None, par2=None):
 
-	name = id2name(name)    
-	print name
-	return render_template('hello.html', name=name)
+	#name, birth, death, par1url, par1, par2url, par2  = id2name(name)    
+	row = id2name(name)    
+	#name, birth, death, par1, par2 = id2name(name)    
+	#print "name", name
+	name, birth, death = row
+	print name, birth, death
+	return render_template('hello.html', name=name, birth=birth, death=death )
 
 
 
@@ -17,22 +21,13 @@ def hello(name=None):
 graph = rdflib.Graph()
 graph.parse('CEpeople.ttl', format= 'turtle')
 
-
-querynames = """
-prefix p: <localhost:3030/ds/person#> 
-
-SELECT ?name 
-WHERE {
-  REPLACEME p:labelname ?name .
-}
-"""
-
-
-
-
-
-
-
+#querynames = """
+#prefix p: <localhost:3030/ds/person#> 
+#SELECT ?name 
+#WHERE {
+#  REPLACEME p:labelname ?name .
+#}
+#"""
 
 
 @app.route('/')
@@ -44,18 +39,24 @@ def id2name(username):
     # show the user profile for that user
 	querynames = """
 	prefix p: <localhost:3030/ds/person#> 
-	SELECT ?name 
+	SELECT ?name ?birth ?death
 	WHERE {
-  		REPLACEME p:labelname ?name .
-	}
-	"""   
+  		REPLACEME p:labelname ?name ;
+		p:birth ?birth ;
+   		p:death ?death .
+	}	
+"""
+
 	querynames = querynames.replace("REPLACEME",username)
 	resultnames = graph.query(querynames)
 #	print 'User %s' % username
 	print querynames
 	for row in resultnames:
-		print 'result %s' % row
-		return '%s' % row
+		print "row", row
+		name, birth, death=  row
+		print name, birth, death
+		return row
+		#return name, birth, death, par1, par2
 
 #	return '??? %s' % username
 
